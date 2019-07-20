fd1d128105b17d3227d8f965bbad4d8cca47b1cd
foltran <jonny_foltran@hotmail.com>
2019-07-20 17:01:43 -0500

    aws

--- lapin.py
+++ lapin.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Post, Songs
+from app.models import User, Post
 
 
 app = create_app()
@@ -8,4 +8,4 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Songs': Songs}
+    return {'db': db, 'User': User, 'Post': Post}
